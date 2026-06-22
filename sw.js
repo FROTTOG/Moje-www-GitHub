134ddbdf2cd20f9c59809021d820b91e9a611379
Jan Minařík <frottokomplex@gmail.com>
2026-06-22 17:18:14 +0200

    První verze webu

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'jmweb-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/konfigurator.html',
+  '/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Zkusí načíst z cache, pokud není, stáhne ze sítě
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
